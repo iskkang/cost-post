d4caf6466e39852749f9e849ab6479e1e70f635c
iskkang <76502863+iskkang@users.noreply.github.com>
2024-09-08 07:22:14 +0900

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.get('/api/tickets', (req, res) => {
 });
 
 // 서버 시작
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
